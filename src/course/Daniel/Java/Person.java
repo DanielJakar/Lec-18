afe1110d41530fc76f63d59d1454fbb1379efe2d
DanielJakar <danieljakar@gmail.com>
2017-03-10 13:06:13 +0200

    Added comparison.

--- src/course/Daniel/Java/Person.java
+++ src/course/Daniel/Java/Person.java
@@ -5,39 +5,67 @@ import course.Daniel.Java.race.Driver;
 /**
  * Created by Jakars on 10/03/2017.
  */
-public class Person implements Comparable {
+public class Person implements Comparable<Person> {
     private String name;
+    private String lastName;
+    int age;
 
-    private Person(String name) {this.name = name; }
+    private Person(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
     //how to overide a private constructor
     //Static method:
     public static Person moshe() {
-        return new Person("Moshe");
+        return new Person("Moshe", "Doe", 22);
     }
 
     public static Person david() {
-        return new Person("David");
+        return new Person("David", "Cohen", 25);
     }
 
-    public static Person itzik() {
-        return new Person("Itzik");
+    public static Person moshe2() {
+        return new Person("Moshe", "Cohen", 23);
+    }
+    public static Person david2() {
+        return new Person("David", "Cohen", 29);
     }
 
 
-    @Override
-    public int compareTo(Object o) {
-        if (o instanceof Person){
-            Person p2 = (Person) o;
+//    @Override
+//    public int compareTo(Object o) {
+//        if (o instanceof Person){
+//            Person p2 = (Person) o;
+//
+//            int diff = name.compareTo(p2.name);
+//            return diff;
+//        }
+//        return 0;
+//    }
 
-            int diff = name.compareTo(p2.name);
-            return diff;
-        }
-        return 0;
-    }
 
     @Override
     public String toString() {
-        return name;
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person p2) {
+        int namediff = name.compareTo(p2.name);
+        if (namediff != 0) return namediff;
+
+        int lastnamediff = lastName.compareTo(p2.lastName);
+        if (lastnamediff != 0) return lastnamediff;
+
+        int agediff = age - p2.age;
+        return agediff;
+
+
     }
 }
