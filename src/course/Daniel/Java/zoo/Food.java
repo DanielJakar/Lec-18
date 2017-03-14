3e50422b4a2df9bee1d4600fb74e2a757b1cbf88
DanielJakar <danieljakar@gmail.com>
2017-03-14 17:32:36 +0200

    Finished?

--- src/course/Daniel/Java/zoo/Food.java
+++ src/course/Daniel/Java/zoo/Food.java
@@ -4,6 +4,24 @@ package course.Daniel.Java.zoo;
  * Created by Jakars on 07/03/2017.
  */
 public enum Food {
-    GRAINS, MEAT, GRASS;
+    GRAINS(1), MEAT(2), GRASS(3);
 
+    private int aVal;
+
+    Food(int aVal) {
+        this.aVal = aVal;
+    }
+
+    public int getaVal() {
+        return aVal;
+    }
+
+    public void setaVal(int aVal) {
+        this.aVal = aVal;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
